6430db8c36481b9b0f8606f21b88ac83e69ec693
zypeh <zypeh.geek@gmail.com>
2019-09-07 01:17:05 +0800

    pcap reader

--- tsuru.hs
+++ tsuru.hs
@@ -1,4 +1,24 @@
 module Main(main) where
 
+import System.Environment (getArgs)
+import Data.Binary.Get ()
+import qualified Data.ByteString as B
+
 main :: IO ()
-main = putStrLn "hello world!"
+main = do
+    args <- getArgs
+    parseArgs args
+    where
+        parseArgs [pcapFileName] = readPcapFile pcapFileName
+        parseArgs [] = putStrLn "No input. Exit now."
+        parseArgs _ = putStrLn "unimplemented"
+
+-- Read quote market 
+-- data MarketData = MarketData
+-- { datatype :: 
+-- }
+
+readPcapFile :: String -> IO ()
+readPcapFile fileName = do 
+    content <- B.readFile fileName
+    print . B.unpack $ content
